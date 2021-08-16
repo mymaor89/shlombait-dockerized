e2a2c617de7349a2c00ee3f52476f8fbf5c7be51
Maor Yatskan <mymaor89@gmail.com>
2021-08-17 01:18:58 +0300

    Change consulting design

--- react-client/src/Components/Consulting/Consulting.js
+++ react-client/src/Components/Consulting/Consulting.js
@@ -18,20 +18,8 @@ class Consulting extends React.Component {
                         alt="UI Designer"
                       />
                       <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="http://gocheckers.com/images/roster/josh_wesley_headshot_1718.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
+                        <h3>Maor Yatskan</h3>
+                        <p>Programmer</p>
                       </div>
                     </div>
                   </div>
@@ -42,102 +30,19 @@ class Consulting extends React.Component {
                         alt="UI Designer"
                       />
                       <div className="member-info">
-                        <h3>John Doe</h3>
+                        <h3>Eden Edri</h3>
                         <p>UI Designer</p>
                       </div>
                     </div>
                   </div>
                 </li>
-                {/* /single member row ends */}
-                {/* single member row starts */}
-                <li className="clearfix">
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="https://axis.org/wp-content/uploads/2015/05/evan-headshot.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="http://gocheckers.com/images/roster/andrew_poturalski_headshot_1718.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="https://axis.org/wp-content/uploads/2015/12/2015-Dougie-Headshot.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                {/* /single member row ends */}
-                {/* single member row starts */}
-                <li className="clearfix">
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="https://axis.org/wp-content/uploads/2014/10/chris-headshot.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="https://axis.org/wp-content/uploads/2014/12/jeremiah-e1418073411753.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div className="member-details">
-                    <div>
-                      <img
-                        src="http://gocheckers.com/images/roster/josh_wesley_headshot_1718.png"
-                        alt="UI Designer"
-                      />
-                      <div className="member-info">
-                        <h3>John Doe</h3>
-                        <p>UI Designer</p>
-                      </div>
-                    </div>
-                  </div>
-                </li>
-                {/* /single member row ends */}
               </ul>
               {/* /end team-photos */}
             </div>
             {/* /end col-md-8 */}
             <div className="pull-left col-md-4 sm-text-center">
               <div className="team-overview">
-                <h2>Who Are We?</h2>
-                <a href="#">Meet the Entire Team</a>
-                <p />
+                <h2>מי אנחנו</h2>
                 <div>
                   Lorem ipsum dolor, sit amet consectetur adipisicing elit.
                   Adipisci quae nesciunt officia! Autem, nam eum voluptatem,
