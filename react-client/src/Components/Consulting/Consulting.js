0cfad3a8f3de3e52e3428c13d800e8adc7a02f0f
Maor Yatskan <mymaor89@gmail.com>
2021-08-20 03:41:51 +0300

    Front end of Contact us form

--- react-client/src/Components/Consulting/Consulting.js
+++ react-client/src/Components/Consulting/Consulting.js
@@ -2,7 +2,23 @@ import "./Consulting.css";
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Maor from "./maor.jpg";
+import { makeStyles } from "@material-ui/core/styles";
+import TextField from "@material-ui/core/TextField";
+import clsx from 'clsx';
+
+
+
 class Consulting extends React.Component {
+  state = {
+    name: "",
+    email: "",
+    message: "",
+  };
+  updateInputValue(e) {
+    let new_state = this.state;
+    new_state[e.target.id] = e.target.value;
+    this.setState({ new_state });
+  }
   render() {
     return (
       <div>
@@ -14,10 +30,7 @@ class Consulting extends React.Component {
                 <li className="clearfix">
                   <div className="member-details">
                     <div>
-                      <img
-                        src={Maor}
-                        alt="UI Designer"
-                      />
+                      <img src={Maor} alt="UI Designer" />
                       <div className="member-info">
                         <h3>Maor Yatskan</h3>
                         <p>Programmer</p>
@@ -43,13 +56,13 @@ class Consulting extends React.Component {
             {/* /end col-md-8 */}
             <div className="pull-left col-md-4 sm-text-center">
               <div className="team-overview">
-                <h2>לעזאזל מי אנחנו</h2>
+                <h2> מי אנחנו</h2>
                 <div>
-                  בוגרי תואר להנדסת תוכנה במכללת סמי שמעון אשדוד.
-                  החלטנו להקים את האתר לשיקום הזוגיות מאחר וזו מטרה חשובה לשלמות החברה ורווחתה.
-                  לאחר חיפוש אחר אתרים אחרים בנושא, גילינו כי אין מענה הולם בחרנו להרים את הכפפה.
-                  האתר עדיין נמצא בשלבי פיתוח שבסיומו תקבלו דרכים יצירתיות לשקם ולפתח את הזוגיות שלכם.
-                  בהצלחה!
+                  בוגרי תואר להנדסת תוכנה במכללת סמי שמעון אשדוד. החלטנו להקים
+                  את האתר לשיקום הזוגיות מאחר וזו מטרה חשובה לשלמות החברה
+                  ורווחתה. לאחר חיפוש אחר אתרים אחרים בנושא, גילינו כי אין מענה
+                  הולם בחרנו להרים את הכפפה. האתר עדיין נמצא בשלבי פיתוח שבסיומו
+                  תקבלו דרכים יצירתיות לשקם ולפתח את הזוגיות שלכם. בהצלחה!
                 </div>
                 <p />
               </div>
@@ -59,6 +72,52 @@ class Consulting extends React.Component {
           {/* /end row */}
         </div>
         {/* /end container */}
+        <div className="contact-us">
+          <form
+            /* onSubmit={registerHandler} */ className="contact-us__form"
+          >
+            <h1 className="contact-us__title">Contact us</h1>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input
+                type="text"
+                required
+                id="name"
+                placeholder="Enter name"
+                value={this.state.name}
+                onChange={(evt) => this.updateInputValue(evt)}
+              />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input
+                type="email"
+                required
+                id="email"
+                placeholder="Email address"
+                value={this.state.email}
+                onChange={(evt) => this.updateInputValue(evt)}
+              />
+            </div>
+            <div className="form-group" className="root">
+              <label htmlFor="message">message:</label>
+              <TextField
+                id="message"
+                required
+                label="Enter message"
+                multiline
+                rows={4}
+                fullWidth
+                defaultValue="Enter message"
+                value={this.state.message}
+                onChange={(evt) => this.updateInputValue(evt)}
+              />
+            </div>
+            <button type="submit" className="btn btn-primary">
+              Send
+            </button>
+          </form>
+        </div>
       </div>
     );
   }
